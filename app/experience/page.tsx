87a295409ad1f737d24001de3ff129745ee057e6
Anand Kashyap <akanandkumar315@gmail.com>
2026-09-15 09:28:10 +0530

    feat: add dedicated experience page

---
+++ app/experience/page.tsx
@@ -0,0 +1,11 @@
+import Header from "@/components/layout/Header";
+import ExperiencePage from "@/components/sections/ExperiencePage";
+
+export default function Experience() {
+    return (
+        <main>
+            <Header />
+            <ExperiencePage />
+        </main>
+    );
+}
